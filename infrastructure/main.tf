9b621d085457ff5d2f96d2c836d39544cd7e0b6f
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2025-03-30 18:37:09 -0500

    Instantiate the app stage in the terraform main

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,3 +18,20 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "this" {
+  name     = "ci-cd-demo-azure-rg"
+  location = "East US 2"
+}
+
+module "todo_app" {
+  source = "./stages/app"
+
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  registry_name     = var.registry_name
+  service_image_tag = var.service_image_tag
+  db_server_name    = var.db_server_name
+  db_password       = var.db_password
+}
---
+++ infrastructure/outputs.tf
@@ -0,0 +1,3 @@
+output "todo_service_outbound_ip_addresses" {
+  value = module.todo_app.service_outbound_ip_addresses
+}
