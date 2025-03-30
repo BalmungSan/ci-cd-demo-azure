46609492240ea656035142ceb0d8540ceb8051fd
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2025-03-30 17:45:50 -0500

    Fix connectivity error between the container and the db

--- infrastructure/modules/db/db.tf
+++ infrastructure/modules/db/db.tf
@@ -12,7 +12,7 @@ resource "azurerm_postgresql_flexible_server" "this" {
   administrator_login    = var.db_user
   administrator_password = var.db_password
 
-  public_network_access_enabled = false
+  public_network_access_enabled = true
 
   lifecycle {
     ignore_changes = [
@@ -26,4 +26,11 @@ resource "azurerm_postgresql_flexible_server_database" "this" {
   server_id = azurerm_postgresql_flexible_server.this.id
   charset   = "UTF8"
   collation = "en_US.utf8"
+}
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "this" {
+  name             = "allow-public-access-from-any-azure-service"
+  server_id        = azurerm_postgresql_flexible_server.this.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
