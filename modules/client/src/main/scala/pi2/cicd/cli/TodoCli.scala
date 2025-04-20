90530200e3953e73a39525087f4505ea090e3651
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2025-04-20 13:11:40 -0500

    Add TodoCli

---
+++ modules/client/src/main/scala/pi2/cicd/cli/TodoCli.scala
@@ -0,0 +1,26 @@
+package co.edu.eafit.dis.pi2.cicd
+package cli
+
+import cats.effect.IO
+
+import service.TodoService
+
+object TodoCli:
+  def make(
+    service: TodoService[IO]
+  ): IO[Unit] =
+    val interpreter = CommandInterpreter(service)
+
+    def prompt(msg: String): IO[String] =
+      IO.print(msg) >> IO.print("> ") >> IO.readLine
+
+    val askForCommand: IO[Command] =
+      prompt(msg = "Please input a command").map(Command.parse)
+
+    val welcomeMessage: IO[Unit] =
+      IO.println("Welcome to the TODO App")
+
+    val loop: IO[Unit] =
+      askForCommand.flatMap(interpreter.runCommand).flatMap(IO.println)
+
+    welcomeMessage >> loop.foreverM
