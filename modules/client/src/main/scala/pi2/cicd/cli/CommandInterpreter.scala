e11c5913ffb458b3062c5bc814d11f06f5c23913
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2025-04-20 13:32:55 -0500

    Implement the Help and Unknown commands

--- modules/client/src/main/scala/pi2/cicd/cli/CommandInterpreter.scala
+++ modules/client/src/main/scala/pi2/cicd/cli/CommandInterpreter.scala
@@ -12,8 +12,11 @@ private[cli] final class CommandInterpreter(
     command match
       case Command.Help =>
         IO.pure(
-          """
-              """
+          """|The following commands are available:
+             |list               -> List all TODOs
+             |add                -> Adds a new TODO
+             |complete <todoId>  -> Completes the given TODO
+          """.stripMargin
         )
 
       case Command.List =>
@@ -27,8 +30,9 @@ private[cli] final class CommandInterpreter(
 
       case Command.Unknown(command) =>
         IO.pure(
-          """
-              """
+          s"""|Unknown command: ${command}
+              |Type help for information on available commands
+           """.stripMargin
         )
   end runCommand
 end CommandInterpreter
